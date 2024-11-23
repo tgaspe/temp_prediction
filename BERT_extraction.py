e7cd419141aa9f6adda2cca108adcede90101ac8
Theodoro Gasperin Terra Camargo <theodoro.gasperinterracamargo@student.kuleuven.be>
2024-11-23 22:53:37 +0100

    debbuging

--- BERT_extraction.py
+++ BERT_extraction.py
@@ -98,12 +98,12 @@ class ProteinEmbeddingGenerator:
                                   return_tensors="pt", 
                                   truncation=True, 
                                   max_length=max_length)
-            logger.info("LLAMA DID I GOT HERE?...")
             
             # Get the input token embeddings
             with torch.no_grad():
                 logger.info("WAZA DID I GOT HERE?...")
                 input_embeddings = self.model.get_input_embeddings()(inputs.input_ids.to(self.device))
+                logger.info(f"LLAMA DID I GOT HERE?...{input_embeddings}")
                 embedding = input_embeddings.mean(dim=1).squeeze().cpu().numpy()
             
             return embedding
